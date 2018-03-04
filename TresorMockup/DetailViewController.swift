c837edc3e42e15cfc254e6810dbb2628ae5d3273
OKU Junichirou <jun1ro1@me.com>
2018-03-04 11:50:05 +0900

    coding

--- TresorMockup/DetailViewController.swift
+++ TresorMockup/DetailViewController.swift
@@ -9,52 +9,24 @@
 import UIKit
 
 enum AppKeyType: Int  {
-    case  safety = 0        //  0
-    case  reason            //  1
-    case  attendability     //  2
-    case  family            //  3
-    case  house             //  4
-    case  cllocation        //  5
-    case  placemark         //  6
-    case  prefecture        //  7
-    case  location          //  8
-    case  surroudings       //  9
-    case  message           // 10
-    case  companyCode       // 11
-    case  employeeCode      // 12
-    case  toAddress1        // 13
-    case  toAddress2        // 14
-    case  ccAddress1        // 15
-    case  ccAddress2        // 16
-    case  mail              // 17
-    case  bcpWeb            // 18
-    case  info              // 19
-    case  typeEnd           // 20
+    case  title = 0
+    case  url
+    case  userid
+    case  password
+    case  selectAt
+    case  memo
+    case  typeEnd
 
     var description: String {
         var s: String = "**UNKNOWN**AppKeyType"
         switch self {
-        case .safety:        s = "Safety"
-        case .reason:        s = "Reason"
-        case .attendability: s = "Attendability"
-        case .family:        s = "Family"
-        case .house:         s = "House"
-        case .cllocation:    s = "CLLocation"
-        case .placemark:     s = "Placemark"
-        case .prefecture:    s = "Prefecture"
-        case .location:      s = "Location"
-        case .surroudings:   s = "Surroudings"
-        case .message:       s = "Message"
-        case .companyCode:   s = "CompanyCode"
-        case .employeeCode:  s = "EmployeeCode"
-        case .toAddress1:    s = "ToAddress1"
-        case .toAddress2:    s = "ToAddress2"
-        case .ccAddress1:    s = "CcAddress1"
-        case .ccAddress2:    s = "CcAddress2"
-        case .mail:          s = "Mail"
-        case .bcpWeb:        s = "BCPWeb"
-        case .info:          s = "Info"
-        case .typeEnd:       s = "TypeEnd"
+        case .title:      s = "Title"
+        case .url:        s = "URL"
+        case .userid:     s = "User ID"
+        case .password:   s = "Password"
+        case .selectAt:   s = "Select at"
+        case .memo:       s = "Memo"
+        case .typeEnd:    s = "TypeEnd"
         }
         return s
     }
@@ -76,23 +48,17 @@ enum AppKeyType: Int  {
 }
 
 fileprivate enum SectionType: Int {
-    case myself = 0
-    case home
-    case place
-    case message
-    case send
-    case bcpWeb
+    case site = 0
+    case account
+    case memo
     case typeEnd
 
     var description: String {
         var s: String = "**UNKNOWN**SectionType"
         switch self {
-        case .myself:  s = "Myself "
-        case .home:    s = "Home   "
-        case .place:   s = "Place  "
-        case .message: s = "Message"
-        case .send:    s = "Send   "
-        case .bcpWeb:  s = "BCPWeb "
+        case .site:    s = "Site   "
+        case .account: s = "Account"
+        case .memo:    s = "Memo  "
         case .typeEnd: s = "TypeEnd"
         }
         return s
@@ -100,12 +66,9 @@ fileprivate enum SectionType: Int {
 
     var header: String {
         return [
-            "I am".localized,
-            "My Home".localized,
-            "Place".localized,
-            "Message".localized,
+            "Site".localized,
+            "Account".localized,
             "",
-            "BCP Web".localized,
             ][self.rawValue]
     }
     static var count: Int { return SectionType.typeEnd.rawValue }
@@ -123,36 +86,30 @@ fileprivate enum SectionType: Int {
 }
 
 fileprivate var layouter = J1Layouter<SectionType, AppKeyType>([
-    .safety:        (section: .myself,  row: 0),
-    .reason:        (section: .myself,  row: 1),
-    .attendability: (section: .myself,  row: 1),
-    .family:        (section: .home,    row: 0),
-    .house:         (section: .home,    row: 1),
-    .prefecture:    (section: .place,   row: 0),
-    .location:      (section: .place,   row: 1),
-    .surroudings:   (section: .place,   row: 2),
-    .message:       (section: .message, row: 0),
-    .mail:          (section: .send,    row: 0),
-    .bcpWeb:        (section: .bcpWeb,  row: 0)
+    .title:        (section: .site,    row: 0),
+    .url:          (section: .site,    row: 1),
+    .userid:       (section: .account, row: 0),
+    .password:     (section: .account, row: 1),
+    .selectAt:     (section: .account, row: 2),
+    .memo:         (section: .memo,    row: 0),
     ])
 
-class DetailViewController: UIViewController {
+class LabelCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel?
+}
 
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
+class TextViewCell: UITableViewCell {
+    @IBOutlet weak var textView: UITextView?
+}
 
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-    //            label.text = detail.timestamp!.description
-            }
-        }
-    }
+class DetailViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.tableView.estimatedRowHeight = 44.0
+        self.tableView.rowHeight          = UITableViewAutomaticDimension
         configureView()
     }
 
@@ -168,6 +125,72 @@ class DetailViewController: UIViewController {
         }
     }
 
+    // MARK: - Table View
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return SectionType.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let sec = SectionType(rawValue: section) else {
+            return nil
+        }
+        return sec.header
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let sec = SectionType(rawValue: section) else {
+            return 0
+        }
+        return layouter.numberOfRows(inSection: sec)
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        let key = layouter.key(forIndexPath: indexPath) ?? .typeEnd
+
+        switch key {
+        case .title:
+            cell = tableView.dequeueReusableCell(withIdentifier: "CellLabel", for: indexPath) as! LabelCell
+            (cell as! LabelCell).label?.text = detailItem?.title
+
+        case .url:
+            cell = tableView.dequeueReusableCell(withIdentifier: "CellLabel", for: indexPath) as! LabelCell
+            (cell as! LabelCell).label?.text = detailItem?.url
+
+        case .userid:
+            cell = tableView.dequeueReusableCell(withIdentifier: "CellLabel", for: indexPath) as! LabelCell
+            (cell as! LabelCell).label?.text = detailItem?.userid
+
+        case .password:
+            cell = tableView.dequeueReusableCell(withIdentifier: "CellLabel", for: indexPath) as! LabelCell
+            (cell as! LabelCell).label?.text = detailItem?.password
+
+        case .selectAt:
+            cell = tableView.dequeueReusableCell(withIdentifier: "CellLabel", for: indexPath) as! LabelCell
+            (cell as! LabelCell).label?.text = "since" + (detailItem?.selectAt?.description ?? "")
+
+        case .memo:
+            cell = tableView.dequeueReusableCell(withIdentifier: "CellTextView", for: indexPath) as! TextViewCell
+            (cell as! TextViewCell).textView?.text = detailItem?.memo
+
+        default:
+            cell = tableView.dequeueReusableCell(withIdentifier: "CellLabel", for: indexPath)
+            assertionFailure()
+
+        }
+
+        return cell
+    }
+    
+    func configureView() {
+
+        // Update the user interface for the detail item.
+//        if let detail = detailItem {
+//            if let label = detailDescriptionLabel {
+//                //            label.text = detail.timestamp!.description
+//            }
+//        }
+    }
 
 }
 
